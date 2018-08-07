42ed2b427581bf4dae02a0b537809878f90a99a4
Morten Poul Jensen <mpj@signifly.com>
2018-08-07 14:14:59 +0200

    Add defintion controller

---
+++ src/Http/Controllers/DefinitionController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
+use Signifly\DefinitionSchema\DefinitionFactory;
+
+class DefinitionController extends Controller
+{
+    public function show(Request $request, $type, $entity)
+    {
+        $definition = (new DefinitionFactory($request, $type, $entity))->make();
+
+        return new JsonResponse($definition->build());
+    }
+}
